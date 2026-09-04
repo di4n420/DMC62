347d72ed0d65da21244e89c9f44c8704e3062924
di4n420 <ferrecciodianap@gmail.com>
2026-09-03 22:26:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ modulos = st.selectbox("Selecione el módulo",["Listas", "Arreglos", "Funciones"
 if modulos == "Listas":
   st.write("Te encuentras en el módulo de Listas")
 
-  valor_inicial = st.number_input("Ingresa tu valor inicial del rango")
-  valor_final = int(st.number_input("Ingresa tu valor final del rango"))
+  valor_inicial = st.number_input("Ingresa tu valor inicial del rango",value=0)
+  valor_final = int(st.number_input("Ingresa tu valor final del rango",value=10))
 
   Lista = list(range(valor_inicial,valor_final))
 
