e773b1e1078bbde7fdfab808052363cb67748fd0
di4n420 <ferrecciodianap@gmail.com>
2026-09-03 22:23:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 import streamlit as st
 
 st.title("Especialización Python for Analytics")
-
 st.sidebar.title("Parámetros")
-
 st.write("Elaborado por: Carlos Carrillo")
+
+modulos = st.selectbox("Selecione el módulo",["Listas", "Arreglos", "Funciones", "POO"])
+
+if modulos == "Listas":
+  st.write("Te encuentras en el módulo de Listas")
+
+  valor_inicial = st.number_input("Ingresa tu valor inicial del rango")
+  valor_final = int(st.number_input("Ingresa tu valor final del rango"))
+
+  Lista = list(range(valor_inicial,valor_final))
+
+  st.write(lista)
+
+elif modulos == "Arreglos":
+  st.write("Te encuentras en el módulo de Arreglos")
+elif modulos == "Funciones":
+  st.write("Te encuentras en el módulo de Funciones")
+  
+else
+  st.write("Te encuentras en el módulo de POO")
